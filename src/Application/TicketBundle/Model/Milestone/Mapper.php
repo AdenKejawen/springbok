4dd92be37476657b27ac47d0c659cf8ed3d3fa6f
Maurice Fonk <info@naneau.nl>
2010-02-24 19:33:10 +0100

    Added milestone service/mapper

---
+++ src/Application/TicketBundle/Model/Milestone/Mapper.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Mapper.php
+ * 
+ * @category        Springbok
+ * @package         TicketBundle
+ * @subpackage      Model
+ */
+
+namespace Application\TicketBundle\Model\Milestone;
+
+use Application\TicketBundle\Model;
+
+/**
+ * Mapper
+ *
+ * DataMapper for milestones
+ *
+ * @category        Springbok
+ * @package         TicketBundle
+ * @subpackage      Model
+ */
+class Mapper extends Mapper
+{
+    public function getById($id)
+    {
+
+    }
+
+    protected function getMongo()
+    {
+        
+    }
+}
---
+++ src/Application/TicketBundle/Service/MilestoneService.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * MilestoneService.php
+ *
+ * @category            Springbok
+ * @package		TicketBundle
+ * @subpackage		Service
+ */
+
+namespace Application\TicketBundle\Service;
+
+use Application\TicketBundle\Model;
+
+/**
+ * MilestoneService
+ *
+ * @category		Springbok
+ * @package		TicketBundle
+ * @subpackage		Service
+ */
+class MilestoneService extends Service
+{
+    /**
+     * get milestone by id
+     *
+     * @param int $id
+     * @return Milestone
+     */
+    public function getById($id)
+    {
+        
+    }
+
+    /**
+     * get mapper
+     *
+     * 
+     */
+    protected function getMapper() {}
+}
