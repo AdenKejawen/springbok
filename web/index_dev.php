08ab16a1e08ba38648cbc4e5d4d7d907c77af924
Maurice Fonk <info@naneau.nl>
2010-02-24 14:03:23 +0100

    And fixed the index_dev to match the new style of layout (with capitals)

--- web/index_dev.php
+++ web/index_dev.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../springbok/SpringbokKernel.php';
+require_once __DIR__.'/../Springbok/SpringbokKernel.php';
 
 $kernel = new SpringbokKernel('dev', true);
 $kernel->run();
