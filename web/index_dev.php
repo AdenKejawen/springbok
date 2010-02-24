01ca4de2315b5907317b04232b5b91ff74b5801b
geoffrey <grosfrais@gmail.com>
2010-02-24 14:27:52 +0100

    fixed a bunch of typos + updated permissions on cache/ and logs/

--- web/index_dev.php
+++ web/index_dev.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../springbok/SpringbokKernel.php';
+require_once __DIR__.'/../springbok/springbokKernel.php';
 
 $kernel = new SpringbokKernel('dev', true);
 $kernel->run();
