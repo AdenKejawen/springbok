7e4d649bbba56890fd9659c492e6e9a4c6aabec3
Maurice Fonk <info@naneau.nl>
2010-02-24 18:51:05 +0100

    added milestone too

---
+++ src/Application/TicketBundle/Model/Milestone.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Milestone.php
+ *
+ * @category        Springbok
+ * @package         SpringbokBundle
+ * @subpackage      Model
+ */
+
+namespace Application\SpringbokBundle\Model;
+
+use Application\SpringbokBundle\Model;
+
+/**
+ * Milestone
+ *
+ * @category        Springbok
+ * @package         SpringbokBundle
+ * @subpackage      Model
+ */
+class Milestone extends DomainObject
+{
+    
+}
