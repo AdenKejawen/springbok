08ea92939850368c7bac14df59a877fa0287f584
Maurice Fonk <info@naneau.nl>
2010-02-24 19:17:26 +0100

    Made ticket service mor clear

--- src/Application/TicketBundle/Service/TicketService.php
+++ src/Application/TicketBundle/Service/TicketService.php
@@ -2,8 +2,8 @@
 /**
  * TicketService.php
  *
- * @category		Springbok
- * @package			TicketBundle
+ * @category            Springbok
+ * @package		TicketBundle
  * @subpackage		Service
  */
 
@@ -14,31 +14,70 @@ use Application\TicketBundle\Model;
 /**
  * TicketService
  *
- * a service to work with tickets
+ * a service to work with tickets, at first this may seem to just add abstraction
+ * but this service as a whole is a reusable component for your controllers, apis,
+ * command line interfaces, etc. It does not have to care about the persistance
+ * layer, which is a job for the datamappers.
  *
  * @category		Springbok
- * @package			TicketBundle
+ * @package		TicketBundle
  * @subpackage		Service
  */
 class TicketService extends Service
 {
     /**
-     * get a ticket by it's id
+     * get Ticket by id
      *
-     * @param int $id 
-     * @return Model\Ticket $ticket
+     * @param int $id
+     * @return \Application\TicketBundle\Model\Ticket
      */
     public function getById($id)
-    {}
+    {
+        return $this->getMapper()->getById($id);
+    }
+
+    /**
+     * get Tickets by tag(s)
+     *
+     * @param string|array $tag
+     * @return array[int]\Application\TicketBundle\Model\Ticket
+     */
+    public function getByTag($tag)
+    {
+        return $this->getMapper()->getByTag($tag);
+    }
+
+    /**
+     * get tickets by user
+     *
+     * @param User $user
+     * @return array[int]\Application\TicketBundle\Model\Ticket
+     */
+    public function getByUser(User $user)
+    {
+        return $this->getMapper()->getByUser($user);
+    }
 
     /**
      * get tickets by milestone
      *
-     * @param Model\Milestone Milestone $milestone
-     * @return array[int]Ticket
+     * @param Milestone $milestone
      */
     public function getByMilestone(Milestone $milestone)
-    {}
+    {
+        return $this->getMapper()->getByMilestone($milestone);
+    }
+
+    /**
+     * save a ticket
+     *
+     * @param Ticket $ticket
+     * @return bool
+     */
+    public function save(Ticket $ticket)
+    {
+        return $this->getMapper()->save($ticket);
+    }
 
     /**
      * get mapper
