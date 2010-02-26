1522ac02e397a9c74149155a84238ec4a88d5be9
Maurice Fonk <info@naneau.nl>
2010-02-26 15:17:56 +0100

    Mongo inserting works in the ticket mapper :D

--- src/Application/SpringbokBundle/Controller/SpringbokController.php
+++ src/Application/SpringbokBundle/Controller/SpringbokController.php
@@ -8,7 +8,15 @@ class SpringbokController extends Controller
 {
   public function indexAction()
   {
-    var_dump($this->container->getService('ticket.mapper'));
+    $ticketService = $this->container->getService('ticket.mapper');
+    
+    $ticket = new \Application\TicketBundle\Model\Ticket();
+    $ticket->reporterName = 'naneau';
+    $ticket->title = 'springbok ftw';
+    $ticket->description = 'Springbok Rules!';
+    $ticketService->save($ticket);
+//    $ticketService->getById(1);
+
     return $this->render('SpringbokBundle:Springbok:index');
   }
 }
--- src/Application/TicketBundle/Model/Ticket/Mapper.php
+++ src/Application/TicketBundle/Model/Ticket/Mapper.php
@@ -2,11 +2,12 @@
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
- */
+*/
 
 namespace Application\TicketBundle\Model\Ticket;
 
-use \Application\SpringbokBundle\Model;
+use \Application\TicketBundle\Model as Model;
+use \Application\UserBundle\Model\User as User;
 
 /**
  * Mapper
@@ -22,29 +23,49 @@ class Mapper
   /**
    * Mongo database connection
    *
-   * @var MongoDB
+   * @var \MongoDB
    */
   protected $mongo;
 
   /**
-   * constructor
+   * mongo collection to the tickets
+   *
+   * @var \MongoCollection
+   */
+  protected $collection;
+
+  /**
+   * mongo auto increment collection
    * 
+   * @var \MongoCollection
+   */
+  protected $increment;
+
+  /**
+   * constructor
+   *
    * @param MongoDB $mongo
    */
-  public function __construct(MongoDB $mongo)
+  public function __construct(\MongoDB $mongo)
   {
     $this->mongo = $mongo;
+
+    $this->collection = $mongo->tickets;
+    //we need constraints, but this will do for now
+    //FIXME add constraints
+
+    $this->increment = $mongo->increments;
   }
 
   /**
    * get Ticket by id
-   * 
-   * @param int $id 
+   *
+   * @param int $id
    * @return \Application\TicketBundle\Model\Ticket
    */
   public function getById($id)
   {
-    return $this->mongo->tickets->findOne(array('_id' => new MongoId($id)), true);
+    return $this->collection->findOne(array('_id' => new MongoId($id)), true);
   }
 
   /**
@@ -63,17 +84,59 @@ class Mapper
    * @param User $user
    * @return array[int]\Application\TicketBundle\Model\Ticket
    */
-  public function getByUser(User $user)
+  public function getByUser(User\User $user)
   {
   }
 
   /**
    * save a ticket
-   * 
+   *
    * @param Ticket $ticket
    * @return bool
    */
-  public function save(Ticket $ticket)
+  public function save(Model\Ticket $ticket)
   {
+    $data = $this->toArray($ticket);
+    
+    if (empty($data['id'])) {
+      $success = $success = $this->collection->insert($data);
+      
+      $ticket->id = (string) $data['_id'];
+      //set the id after insert
+    } else {
+      $success = $this->collection->save($data);
+    }
+    
+    return $success;
+  }
+
+  /**
+   * parse from domainobject to mongo array notation
+   *
+   * @param Model\Ticket $ticket
+   * @return array
+   */
+  protected function toArray(Model\Ticket $ticket)
+  {
+    $data = array();
+
+    foreach(get_object_vars($ticket) as $key => $val)
+    {
+      $data[$key] = $val;
+      //FIXME we need filtering from camelCase to under_score
+    }
+
+    return $data;
+  }
+
+  /**
+   * parse from array to DomainObject
+   *
+   * @param array $data
+   * @return Model\Ticket
+   */
+  protected function fromArray(array $data)
+  {
+
   }
 }
