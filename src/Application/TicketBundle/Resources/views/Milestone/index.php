44ddaf2c0f8338c588011be53b0ec13212153365
naneau <info@naneau.nl>
2010-02-27 17:18:06 +0100

    added small navigational help to the links

--- src/Application/TicketBundle/Resources/views/Milestone/index.php
+++ src/Application/TicketBundle/Resources/views/Milestone/index.php
@@ -11,12 +11,19 @@ $view->extend('SpringbokBundle::layout');
 //we are using the main layout
 
 ?>
-<ul>
+
+<h2>Milestones</h2>
+
+<ul class="milestones">
+
   <?php foreach($milestones as $milestone ) : ?>
   <li>
-    <a href="<?php echo $view->router->generate('tickets_per_milestone', array('milestone' => $milestone->id)) ?>">
-      <?php echo $milestone->name; ?>
-    </a>
+    <div class="name">
+      <a href="<?php echo $view->router->generate('tickets_per_milestone', array('milestone' => $milestone->id)) ?>">
+        <?php echo $milestone->name; ?>
+      </a>
+    </div>
   </li>
   <?php endforeach ?>
+
 </ul>
--- src/Application/TicketBundle/Resources/views/Milestone/milestone.php
+++ src/Application/TicketBundle/Resources/views/Milestone/milestone.php
@@ -5,14 +5,22 @@
  */
 
 ?>
+
+<a href="<?php echo $view->router->generate('milestone_index') ?>">
+  Milestones
+</a>
+
 <h2><?php echo $milestone->name; ?></h2>
 <?php echo $milestone->description; ?>
 
 <h3>Tickets</h3>
+
 <ul>
+
   <?php foreach($milestone->tickets as $ticket) : ?>
   <li>
     <?php echo $ticket->title; ?>
   </li>
   <?php endforeach; ?>
+  
 </ul>
