9b67f3c360d608e1ea4664f29ff5dd0da41b19df
naneau <info@naneau.nl>
2010-02-28 16:25:18 +0100

    using constructor injection for milestone service's ticketmapper

--- src/Application/TicketBundle/Service/MilestoneService.php
+++ src/Application/TicketBundle/Service/MilestoneService.php
@@ -22,29 +22,30 @@ use Application\TicketBundle\Model\Ticket\Mapper as TicketMapper;
  */
 class MilestoneService
 {
-    /**
-   * ticket mapper
+  /**
+   * milestone mapper
    *
    * @var Mapper
    */
   protected $mapper;
 
   /**
+   * ticket mapper
+   * 
+   * @var TicketMapper
+   */
+  protected $ticketMapper;
+
+  /**
    * constructor
    *
    * @param Mapper $mapper
    */
-  public function __construct(Mapper $mapper)
+  public function __construct(Mapper $mapper, TicketMapper $ticketMapper)
   {
     $this->mapper = $mapper;
+    $this->ticketMapper = $ticketMapper;
   }
-
-  protected $ticketMapper;
-  public function setTicketMapper(TicketMapper $mapper)
-  {
-    $this->ticketMapper = $mapper;
-  }
-
 
   /**
    * get all milestones
