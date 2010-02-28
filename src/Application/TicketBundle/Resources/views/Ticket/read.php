73c112692ac2cfacab537e60d7595d36bf052fd7
naneau <info@naneau.nl>
2010-02-28 22:14:00 +0100

    added simple layout

--- src/Application/TicketBundle/Resources/views/Ticket/read.php
+++ src/Application/TicketBundle/Resources/views/Ticket/read.php
@@ -7,31 +7,39 @@
  * @subpackage    View
  */
 
+$view->extend('SpringbokBundle::layout');
+//we are using the main layout
+
 ?>
 
 <div class="ticket" id="ticket-<?php echo $ticket->id; ?>">
+  <div class="content">
+    <h2><?php echo $ticket->title; ?></h2>
 
-  <h2><?php echo $ticket->title; ?></h2>
+    <?php echo $ticket->description; ?>
 
-  <?php echo $ticket->description; ?>
+    <div class="reporter">
+      <h3>Reported by</h3>
+      <?php echo $ticket->reporterName; ?>
+    </div>
 
-  <div class="reporter">
-    <h3>Reported by</h3>
-    <?php echo $ticket->reporterName; ?>
+    <div class="assignees">
+      <h3>Assigned to</h3>
+      <span>
+        <?php echo implode(', ', $ticket->assignedTo->getRawValue()); ?>
+      </span>
+    </div>
+
+    <div class="tags">
+      <h3>Tags</h3>
+      <span>
+        <?php echo implode(', ', $ticket->tags->getRawValue()); ?>
+      </span>
+    </div>
   </div>
 
-  <div class="assignees">
-    <h3>Assigned to</h3>
-    <span>
-      <?php echo implode(', ', $ticket->assignedTo->getRawValue()); ?>
-    </span>
-  </div>
+  <div class="comments">
+    <h2>Comments (8)</h2>
 
-  <div class="tags">
-    <h3>Tags</h3>
-    <span>
-      <?php echo implode(', ', $ticket->tags->getRawValue()); ?>
-    </span>
   </div>
-
 </div>
