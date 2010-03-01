4f5af4ed4102d9a275ffd3de36433cf6f344bce5
naneau <info@naneau.nl>
2010-03-01 13:55:40 +0100

    added utility functions to user DO, that help with hashing/salting/securing

--- src/Application/UserBundle/Model/User.php
+++ src/Application/UserBundle/Model/User.php
@@ -40,6 +40,13 @@ class User
   public $password;
 
   /**
+   * salt for the password
+   * 
+   * @var string
+   */
+  public $salt;
+
+  /**
    * email
    *
    * @var string
@@ -62,5 +69,53 @@ class User
   public function hasRole($role)
   {
     return in_array($role, $this->roles);
+  }
+
+
+  /**
+   * check wether a password matches the hashed password
+   *
+   * @param string $password
+   */
+  public function passwordMatches($password)
+  {
+    return self::hash($password, $this->salt) == $this->password;
+  }
+
+  /**
+   * set a new password and hash with a random
+   * @param string $newPassword
+   */
+  public function setAndHashPassword($newPassword)
+  {
+    $this->generateSalt();
+
+    $this->password = self::hash($newPassword, $this->salt);
+  }
+
+  /**
+   * generate a random salt
+   *
+   * @return void
+   */
+  protected function generateSalt()
+  {
+    $this->salt = '';
+
+    $alNum = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    for ($x = 0; $x < 20; $x++)
+    {
+      $this->salt .= $alNum[rand(0, 36)];
+    }
+  }
+
+  /**
+   * hash a password with salt
+   *
+   * @return string
+   */
+  protected static function hash($password, $salt)
+  {
+    return md5($password . $salt);
   }
 }
