1fa82ccc7da45433ed6d3522f5c3dc41594d8d74
naneau <info@naneau.nl>
2010-03-01 14:18:03 +0100

    added dashboard controller

---
+++ src/Application/SpringbokBundle/Controller/DashboardController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * DashboardController
+ *
+ * @category        Springbok
+ * @package         SpringbokBundle
+ * @subpackage      Controller
+ */
+
+namespace Application\SpringbokBundle\Controller;
+
+use Symfony\Framework\WebBundle\Controller;
+use Symfony\Components\RequestHandler\Response;
+
+/**
+ * DashboardController
+ *
+ * @category        Springbok
+ * @package         SpringbokBundle
+ * @subpackage      Controller
+ */
+class DashboardController extends Controller
+{
+  /**
+   * dashboard index
+   *
+   * @return Response
+   */
+  public function indexAction()
+  {
+
+  }
+}
