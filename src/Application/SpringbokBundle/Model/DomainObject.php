e67fb714ab16932e8e5a4e9f35fa35aa7258a8bd
geoffrey <grosfrais@gmail.com>
2010-03-01 21:32:19 +0100

    also fixed indentation

--- src/Application/SpringbokBundle/Model/DomainObject.php
+++ src/Application/SpringbokBundle/Model/DomainObject.php
@@ -18,40 +18,40 @@ namespace Application\SpringbokBundle\Model;
  */
 class DomainObject
 {
-    /**
-     * overloaded getter
-     *
-     * @param string $name
-     */
-    public function __get($name)
-    {
-      $funcName = 'get' . ucfirst($name);
-      //check if getName() exists then use that, as a kind of poor man's property
+  /**
+   * overloaded getter
+   *
+   * @param string $name
+   */
+  public function __get($name)
+  {
+    $funcName = 'get' . ucfirst($name);
+    //check if getName() exists then use that, as a kind of poor man's property
 
-      if (!method_exists($this, $funcName)) {
-        throw new Excepton('Property ' . $name . ' not found in object of class ' . get_class($this));
-      }
-
-      return $this->$funcName();
+    if (!method_exists($this, $funcName)) {
+      throw new Excepton('Property ' . $name . ' not found in object of class ' . get_class($this));
     }
 
-    /**
-     * setter
-     * 
-     * @param string $name
-     * @param mixed $value
-     */
-    public function __set($name, $value)
-    {
-      $funcName = 'set' . ucfirst($what);
+    return $this->$funcName();
+  }
 
-      if (method_exists($this, $funcName))
-      {
-        $this->$funcName($value);
-      }
-      else
-      {
-        $this->$name = $value;
-      }
+  /**
+   * setter
+   * 
+   * @param string $name
+   * @param mixed $value
+   */
+  public function __set($name, $value)
+  {
+    $funcName = 'set' . ucfirst($what);
+
+    if (method_exists($this, $funcName))
+    {
+      $this->$funcName($value);
     }
+    else
+    {
+      $this->$name = $value;
+    }
+  }
 }
